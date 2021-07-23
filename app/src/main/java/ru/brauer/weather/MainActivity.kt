b7db540cc55b0be8cc5481d259b397997d9a1c83
PivIgor <brauer.igor@gmail.com>
2021-07-23 23:30:05 +0300

    Added a button and its action when pressed.

--- app/src/main/java/ru/brauer/weather/MainActivity.kt
+++ app/src/main/java/ru/brauer/weather/MainActivity.kt
@@ -1,11 +1,18 @@
 package ru.brauer.weather
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val someButton = findViewById<Button>(R.id.some_button)
+        someButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "The button has pressed", Toast.LENGTH_LONG).show()
+        }
     }
 }
