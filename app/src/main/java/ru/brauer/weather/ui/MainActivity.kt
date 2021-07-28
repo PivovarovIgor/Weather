28859c4efc055c7d324cc01adac7019b6e5535b9
PivIgor <brauer.igor@gmail.com>
2021-07-28 21:13:16 +0300

    Cleaned code after homework01.

--- app/src/main/java/ru/brauer/weather/ui/MainActivity.kt
+++ app/src/main/java/ru/brauer/weather/ui/MainActivity.kt
@@ -16,51 +16,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val weather = Weather("Moscow", 20)
-
-        val (city, temp) = weather
-
-        val someButton = findViewById<Button>(R.id.some_button)
-        someButton.setOnClickListener {
-            Toast.makeText(
-                this@MainActivity,
-                "In $city temperature is $temp ${weather.copy()}",
-                Toast.LENGTH_LONG
-            )
-                .show()
-
-            val weathers = repository.weathers;
-
-            for (currentWeather in weathers) {
-                println("for (currentWeather in data) ${currentWeather.city} : ${currentWeather.temperature}")
-            }
-
-            for (index in 1 until weathers.size) {
-                println("for (index in 1 until data.size) " + weathers[index])
-            }
-
-            for (index in weathers.size - 1 downTo 0) {
-                println("for (index in data.size - 1 downTo 0) " + weathers[index])
-            }
-
-            for (index in weathers.indices) {
-                println("for (index in data.indices) " + weathers[index])
-            }
-
-            for ((index, value) in weathers.withIndex()) {
-                println("for ((index, value) in data.withIndex()) $index : $value")
-            }
-
-            weathers.forEach(::println)
-        }
-
-        val weathersListView = findViewById<RecyclerView>(R.id.weathers_list)
-        val divider = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
-        ContextCompat.getDrawable(this, R.drawable.ic_launcher_background)
-            ?.let(divider::setDrawable)
-        weathersListView.addItemDecoration(divider)
-        weathersListView.adapter = WeatherListAdapter(repository)
     }
 
     companion object {
