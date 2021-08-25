a2a8aa36803578fe80177fedb34d0188bb2ee3a5
PivIgor <brauer.igor@gmail.com>
2021-08-26 00:17:46 +0300

    Search address on map.

--- app/src/main/java/ru/brauer/weather/ui/map/MapsFragment.kt
+++ app/src/main/java/ru/brauer/weather/ui/map/MapsFragment.kt
@@ -1,45 +1,84 @@
 package ru.brauer.weather.ui.map
 
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import ru.brauer.weather.R
+import ru.brauer.weather.databinding.FragmentMapsBinding
+import java.io.IOException
 
 class MapsFragment : Fragment() {
 
+    private lateinit var map: GoogleMap
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        map = googleMap
+        val initPlace = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(initPlace).title("Start_marker"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(initPlace))
     }
+
+    private var _binding: FragmentMapsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+    ): View {
+        _binding = FragmentMapsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+        initSearchByAddress()
+    }
+
+    private fun initSearchByAddress() {
+        binding.buttonSearch.setOnClickListener {
+            val geocoder = Geocoder(it.context)
+            val searchText = binding.searchAddress.text.toString()
+            Thread {
+                try {
+                    val addresses = geocoder.getFromLocationName(searchText, 1)
+                    if (addresses.isNotEmpty()) {
+                        goToAddress(addresses, it, searchText)
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }.start()
+        }
+    }
+
+    private fun goToAddress(addresses: List<Address>, view: View?, searchText: String) {
+        val location = LatLng(addresses.first().latitude, addresses.first().longitude)
+        view?.post {
+            setMarker(location, searchText)
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15f))
+        }
+    }
+
+    private fun setMarker(location: LatLng, searchText: String) {
+        MarkerOptions()
+            .position(location)
+            .title(searchText)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
